ba35fa6623c010e52e549549c077c571d19e7185
Elif Cetin <xxelifcetinxx@gmail.com>
2025-03-22 18:47:25 +0300

    tests writed

--- cypress/e2e/loginform.cy.js
+++ cypress/e2e/loginform.cy.js
@@ -1,5 +1,37 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login Test', () => {
+  beforeEach(() => {
+      cy.visit("http://localhost:5174");
+    });
+  
+  it('Form doldurulduğunda submit edilir', () => {
+   
+    cy.get('input[name="email"]').type('test@example.com');
+    cy.get('input[name="password"]').type('Password123');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/home'); 
+  });
+
+  it('Email yanlış:', () => {
+      
+      cy.get('input[name="email"]').type('hfhf@');
+      cy.get('[data-cy="error-message"]').should("have.length", 1);
+      cy.get('button[type="submit"]').should('be.disabled');
   })
-})
+
+  it('email ve password yanlış:', () => {
+      
+      cy.get('input[name="email"]').type('abab@');
+      cy.gety('input[name="password"]').type('123');
+      cy.get('[data-cy="error-message"]').should("have.length", 2);
+      cy.get('button[type="submit"]').should('be.disabled');
+  })
+
+  it('email ve password doğru fakat check me out işaretlenmedi:', () => {
+      
+      cy.get('input[name="email"]').type('test@example.com');
+      cy.gety('input[name="password"]').type('Password123');
+      cy.get('input[name="terms"]').uncheck();
+      cy.get('[data-cy="error-message"]').should("have.length", 1);
+      cy.get('button[type="submit"]').should('be.disabled');
+  })
+});
