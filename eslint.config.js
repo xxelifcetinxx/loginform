1d18694573c4e5d273f85c8f8d55c0050b246e2e
Elif Cetin <xxelifcetinxx@gmail.com>
2025-03-22 18:51:26 +0300

    eslint cypress config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,29 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    cypress: true, // Cypress ortamını ekleyin
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:cypress/recommended", // Cypress yapılandırmasını ekleyin
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "cypress"], // Cypress eklentisini ekleyin
+  rules: {
+    // İsteğe bağlı olarak ek kurallar ekleyebilirsiniz
+  },
+};
 export default [
   { ignores: ['dist'] },
   {
