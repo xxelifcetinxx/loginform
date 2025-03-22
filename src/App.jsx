a771b8ba805b0b85698608d8b8e6e3e55ab7139c
Elif Cetin <xxelifcetinxx@gmail.com>
2025-03-22 17:59:40 +0300

    anasayfa ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+import React from "react";
+ import "./App.css";
+ import "bootstrap/dist/css/bootstrap.min.css";
+ import Login from "./components/login";
+ import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+ import Success from "./components/success";
+ 
+ function App() {
+   return (
+     <div className="app">
+       <Switch>
+         <Route exact path="/">
+           <p>LOGINSAYFASI</p>
+           <Login />
+         </Route>
+         <Route path="/home">
+           <Success />
+         </Route>
+       </Switch>
+     </div>
+   );
+ }
+ 
+ export default App;
---
+++ src/components/Success.jsx
@@ -0,0 +1,8 @@
+export default function Success() {
+    return (
+      <div>
+        <h1>HOSGELDIN ELF</h1>
+        <p>ANASAYFA </p>
+      </div>
+    );
+  }
